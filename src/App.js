e403818a76d1f523a9b77bc74ba9b82b860a8ee1
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-05-18 22:39:47 -0400

    build component files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.subject}</p>
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.desc}</p>
-      </article>
-    );
-  }
-}
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 
 class App extends Component {
   render() {
