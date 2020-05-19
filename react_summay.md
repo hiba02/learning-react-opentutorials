6bc2e6f97eba5a535dc13fc33c6d4594596c271c
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-05-18 20:07:27 -0400

    learning props

---
+++ react_summay.md
@@ -0,0 +1,19 @@
+## Date: 5.17.20
+
+## For build
+
+```
+npm run build
+```
+
+A new build folder is created.
+
+## simple server
+
+```
+1. npm i -g serve
+or
+2. npx serve -s build
+```
+
+-s: root directory will assign build directory
--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.subject}</p>
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.desc}</p>
+      </article>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App">Hello React!!</div>;
+    return (
+      <div className="App">
+        <Subject title="Web" subject="World Wide Web" />
+        <Subject title="React" subject="For UI" />
+        <TOC />
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language." />
+      </div>
+    );
   }
 }
 
